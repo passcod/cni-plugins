78574f4c15636e0a100dcadc6754228016f822f4
Félix Saparelli <felix@passcod.name>
2021-04-02 01:56:42 +1300

    Add Cni::into_inputs to flatten the main enum when needed

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -72,6 +72,16 @@ impl AsRef<str> for Command {
 }
 
 #[derive(Clone, Debug)]
+pub struct Inputs {
+	pub command: Command,
+	pub container_id: String,
+	pub ifname: String,
+	pub netns: Option<PathBuf>,
+	pub path: Vec<PathBuf>,
+	pub config: NetworkConfig,
+}
+
+#[derive(Clone, Debug)]
 pub enum Cni {
 	Add {
 		container_id: String,
@@ -250,6 +260,54 @@ impl Cni {
 			Err(CniError::Incompatible(version.clone()))
 		} else {
 			Ok(())
+		}
+	}
+
+	pub fn into_inputs(self) -> Option<Inputs> {
+		let command = match &self {
+			Cni::Add { .. } => Command::Add,
+			Cni::Del { .. } => Command::Del,
+			Cni::Check { .. } => Command::Check,
+			Cni::Version(_) => return None,
+		};
+
+		match self {
+			Cni::Add {
+				container_id,
+				ifname,
+				netns,
+				path,
+				config,
+			}
+			| Cni::Check {
+				container_id,
+				ifname,
+				netns,
+				path,
+				config,
+			} => Some(Inputs {
+				command,
+				container_id,
+				ifname,
+				netns: Some(netns),
+				path,
+				config,
+			}),
+			Cni::Del {
+				container_id,
+				ifname,
+				netns,
+				path,
+				config,
+			} => Some(Inputs {
+				command,
+				container_id,
+				ifname,
+				netns,
+				path,
+				config,
+			}),
+			Cni::Version(_) => unreachable!(),
 		}
 	}
 
