7e4683672747ac76dfc1dbcc5560626bf314d005
Félix Saparelli <felix@passcod.name>
2021-04-04 02:28:56 +1200

    Document the logger

--- plugin/src/logger.rs
+++ plugin/src/logger.rs
@@ -1,5 +1,22 @@
 use std::{fs::OpenOptions, path::{Path, PathBuf}};
 
+/// Install the standard logger for plugins
+///
+/// This logger always emits `warn` and `error` level messages to STDERR, and
+/// emits all messages from `debug` level up to a log file in development and
+/// when the **release-logs** feature is enabled.
+///
+/// In development (when `debug_assertions` are enabled), it logs to the current
+/// working directory, and otherwise logs to `/var/log/cni/logname.log`,
+/// creating the directory if it does not exist.
+///
+/// # Panics
+/// - if the working directory cannot be obtained (in development only);
+/// - if the logging directory cannot be created (in development or with the
+///   release-logs feature only);
+/// - if the logfile cannot be opened (in development or with the release-logs
+///   feature only);
+/// - if the logger cannot be installed.
 pub fn install_logger(logname: impl AsRef<Path>) {
 	use simplelog::*;
 
