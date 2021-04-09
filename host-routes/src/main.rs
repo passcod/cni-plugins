dd3cfb207d80b379a3a624c06d8b1bfe32bb9ca9
Félix Saparelli <felix@passcod.name>
2021-04-09 21:42:03 +1200

    Implement most of host-routes... just the routing part todo

--- host-routes/src/main.rs
+++ host-routes/src/main.rs
@@ -1,10 +1,16 @@
-use async_std::task::block_on;
-use cni_plugin::{
-	error::CniError,
-	reply::{reply, SuccessReply},
-	Cni, Command,
+use std::{
+	net::IpAddr,
+	time::{Duration, Instant},
 };
+
+use async_std::{
+	future::timeout,
+	task::{block_on, spawn_blocking},
+};
+use cni_plugin::{Cni, Command, Inputs, error::CniError, reply::{reply, SuccessReply}};
+use ipnetwork::IpNetwork;
 use log::{debug, error, info};
+use serde::{Deserialize, Serialize};
 
 fn main() {
 	cni_plugin::install_logger(env!("CARGO_PKG_NAME"));
@@ -14,36 +20,105 @@ fn main() {
 		env!("CARGO_PKG_VERSION")
 	);
 
-	let cni = Cni::load();
+	// UNWRAP: unreachable due to using load()
+	let Inputs {
+		command, config, ..
+	} = Cni::load().into_inputs().unwrap();
 
-	let (command, config) = match cni {
-		Cni::Add { config, .. } => (Command::Add, config),
-		Cni::Del { config, .. } => (Command::Del, config),
-		Cni::Check { config, .. } => (Command::Check, config),
-		Cni::Version(_) => unreachable!(),
-	};
 	let cni_version = config.cni_version.clone(); // for error
 	info!(
 		"{} serving spec v{} for command={:?}",
 		env!("CARGO_PKG_NAME"),
-		cni_version, command
+		cni_version,
+		command
 	);
 
 	let res: Result<SuccessReply, CniError> = block_on(async move {
-		debug!("config={:#?}", config);
+		if matches!(command, Command::Check) {
+			return Err(CniError::Generic("TODO".into()));
+		}
 
-		match command {
-			Command::Add => Err(CniError::Generic("TODO".into())),
-			Command::Del => Ok(SuccessReply {
-				cni_version: config.cni_version,
-				interfaces: Default::default(),
-				ips: Default::default(),
-				routes: Default::default(),
-				dns: Default::default(),
-				specific: Default::default(),
-			}),
-			Command::Check => Err(CniError::Generic("TODO".into())),
-			Command::Version => unreachable!(),
+		let expr = config
+			.specific
+			.get("routing")
+			.ok_or(CniError::MissingField("routing"))
+			.and_then(|val| {
+				val.as_str().ok_or_else(|| CniError::InvalidField {
+					field: "routing",
+					expected: "string",
+					value: val.clone(),
+				})
+			})?
+			.to_owned();
+		debug!("routing={:?}", expr);
+
+		let input = serde_json::to_string(&config)?;
+
+		debug!("spawning jq");
+		let pre = Instant::now();
+		let eval: String = timeout(
+			Duration::from_secs(1),
+			spawn_blocking(move || jq_rs::run(&expr, &input).map_err(|err| err.to_string())),
+		)
+		.await
+		.map_err(|err| CniError::Generic(format!("jq evaluation timed out: {}", err)))?
+		.map_err(CniError::Generic)?;
+
+		info!("ran jq expression in {:?}", pre.elapsed());
+		debug!("jq eval={:?}", eval);
+
+		let routing: Vec<Routing> = serde_json::from_str(&eval)?;
+		info!("got {} routings from jq expression", routing.len());
+
+		let mut errors = Vec::with_capacity(routing.len());
+		let mut applied = Vec::with_capacity(routing.len());
+		for route in routing {
+			if let Err(err) = if matches!(command, Command::Del) {
+				debug!("deleting {:?}", route);
+				route.del()
+			} else {
+				debug!("adding {:?}", route);
+				route.add()
+			} {
+				errors.push(err);
+			} else {
+				info!("applied route to {}", route.prefix);
+				applied.push(serde_json::to_value(route)?);
+			}
+		}
+
+		if errors.is_empty() {
+			let cni_version = config.cni_version.clone();
+			let mut reply = config
+				.prev_result
+				.map(|val| serde_json::from_value(val).map_err(CniError::Json))
+				.transpose()?
+				.unwrap_or_else(|| SuccessReply {
+					cni_version,
+					interfaces: Default::default(),
+					ips: Default::default(),
+					routes: Default::default(),
+					dns: Default::default(),
+					specific: Default::default(),
+				});
+
+			if let Some(routes) = reply
+				.specific
+				.get_mut("hostRoutes")
+				.and_then(|val| val.as_array_mut())
+			{
+				routes.extend(applied);
+			}
+
+			Ok(reply)
+		} else {
+			Err(CniError::Generic(
+				errors
+					.iter()
+					.map(|e| e.to_string())
+					.collect::<Vec<String>>()
+					.join("\n"),
+			))
 		}
 	});
 
@@ -53,5 +128,26 @@ fn main() {
 			error!("error: {}", res);
 			reply(res.into_reply(cni_version))
 		}
+	}
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Routing {
+	pub prefix: IpNetwork,
+	#[serde(default, skip_serializing_if = "Option::is_none")]
+	pub device: Option<String>,
+	#[serde(default, skip_serializing_if = "Option::is_none")]
+	pub gateway: Option<IpAddr>,
+}
+
+impl Routing {
+	pub fn add(&self) -> Result<(), CniError> {
+		// TODO: add route, replacing existing entry if present
+		Ok(())
+	}
+
+	pub fn del(&self) -> Result<(), CniError> {
+		// TODO: delete route
+		Ok(())
 	}
 }
